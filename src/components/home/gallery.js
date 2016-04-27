ccf6b61e18dd279255e0a6e00b8153aaee04dc3d
Dennis Jefferson <dennismjefferson@gmail.com>
2016-04-27 16:50:51 -0700

    Overlay v0.1

--- src/components/home/gallery.js
+++ src/components/home/gallery.js
@@ -16,11 +16,11 @@ export class SignIn extends Component {
 
   render() {
     const elements = [
-      { src: 'images/home_gallery/photo4.jpg', topText: 'Matt, Ryan & Kyle', bottomText: 'Family, Los Angeles' },
-      { src: 'images/home_gallery/photo2.jpg' },
-      { src: 'images/home_gallery/photo1.jpg' },
-      { src: 'images/home_gallery/photo6.jpg' },
-      { src: 'images/home_gallery/photo5.jpg' }
+      { src: 'images/home_gallery/photo4.jpg', topText: 'Milo', bottomText: 'Portrait, Los Angeles' },
+      { src: 'images/home_gallery/photo2.jpg', topText: 'Lara Croft', bottomText: 'Portrait, San Francisco Coast' },
+      { src: 'images/home_gallery/photo1.jpg', topText: 'Matt, Ryan & Kyle', bottomText: 'Family, San Diego' },
+      { src: 'images/home_gallery/photo6.jpg', topText: 'Sila, Rebecca & Milo', bottomText: 'Family, Los Angeles' },
+      { src: 'images/home_gallery/photo5.jpg', topText: 'Laura Vance', bottomText: 'Portrait, Los Angeles' }
     ];
     return (
       <div className="">
@@ -30,6 +30,7 @@ export class SignIn extends Component {
               const image = index % 2 ? null : (
                 <div key={index} className="image__container" >
                   <img src={element.src} />
+                  <div className="overlay"><div className="overlay__content"><p>{element.topText}</p><span></span><p>{element.bottomText}</p></div></div>
                 </div>
               );
               return image;
@@ -42,6 +43,7 @@ export class SignIn extends Component {
               const image = index % 2 ? (
                 <div key={index} className="image__container" >
                   <img src={element.src} />
+                  <div className="overlay"><div className="overlay__content"><p>{element.topText}</p><span></span><p>{element.bottomText}</p></div></div>
                 </div>
               ) : null;
               return image;
@@ -53,6 +55,6 @@ export class SignIn extends Component {
   }
 }
 
-// <div className="overlay"><p>{element.topText}</p><span></span><p>{element.bottomText}</p></div>
+
 
 export default connect(null, authActions)(SignIn);

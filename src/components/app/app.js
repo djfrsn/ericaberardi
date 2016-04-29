790a41a10226e50f1c8f42ed3830423e3fdc5a26
Dennis Jefferson <dennismjefferson@gmail.com>
2016-04-28 20:00:05 -0700

    Abstract header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import Header from '../header/header';
 import { connect } from 'react-redux';
 import { POST_LOGIN_PATH } from 'config';
 import { authActions } from 'core/auth';
@@ -40,23 +41,7 @@ export class App extends Component {
 
     return (
       <div>
-        <header className="header">
-          <div className="g-row">
-            <div className="g-col">
-              <a href="/" className="home__link">
-                <h1 className="header__title">Erica Berardi</h1>
-                <h2 className="header__sub_title">Photography</h2>
-              </a>
-              <ul className="header__links">
-                <li><a className="header__link" href="/galleries">Galleries</a></li>
-                <li><a className="header__link" href="/news-reporting">News Reporting</a></li>
-                <li><a className="header__link" href="/about">About</a></li>
-                <li><a className="header__link" href="/contact">Contact</a></li>
-                {auth.authenticated ? <li><a className="header__link" onClick={this.signOut} href="#">Sign out</a></li> : null}
-              </ul>
-            </div>
-          </div>
-        </header>
+        <Header />
 
         <main className="main">{children}</main>
 

a2bf119577b95af9936e348a2f73ef6be9c474f3
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-01 00:48:14 -0700

    Position edit icon

--- src/components/home/gallery.js
+++ src/components/home/gallery.js
@@ -1,14 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { galleryActions } from 'core/galleries';
+import { authActions } from 'core/auth';
 
 export class Gallery extends Component {
   static propTypes = {
-    galleries: PropTypes.object.isRequired
+    galleries: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
   }
 
   render() {
     const { homeGallery } = this.props.galleries;
+    const { auth } = this.props;
+    const editIcon = auth.authenticated ? (<i className="fa fa-pencil-square-o gallery-edit__icon" aria-hidden="true"></i>) : null;
     return (
       <div className="">
         <div className="gallery-left">
@@ -17,7 +21,10 @@ export class Gallery extends Component {
               return element ? (
                 <div key={index} className="image__container" >
                   <img src={element.src} />
-                  <div className="overlay"><div className="overlay__content"><p>{element.topText}</p><span></span><p>{element.bottomText}</p></div></div>
+                  <div className="overlay"><div className="overlay__content">
+                    {editIcon}
+                    <p>{element.topText}</p><span></span><p>{element.bottomText}</p>
+                  </div></div>
                 </div>
               ) : null;
             })
@@ -29,7 +36,10 @@ export class Gallery extends Component {
               return element ? (
                 <div key={index} className="image__container" >
                   <img src={element.src} />
-                  <div className="overlay"><div className="overlay__content"><p>{element.topText}</p><span></span><p>{element.bottomText}</p></div></div>
+                  <div className="overlay"><div className="overlay__content">
+                    {editIcon}
+                    <p>{element.topText}</p><span></span><p>{element.bottomText}</p>
+                  </div></div>
                 </div>
               ) : null;
             })
@@ -40,8 +50,7 @@ export class Gallery extends Component {
   }
 }
 
-
-
 export default connect(state => ({
-  galleries: state.galleries
-}), galleryActions)(Gallery);
+  galleries: state.galleries,
+  auth: state.auth
+}), Object.assign({}, authActions, galleryActions))(Gallery);

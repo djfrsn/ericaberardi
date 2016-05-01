479a5c0d110e2a2e29ae102cb5057c61003799bb
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-01 03:13:03 -0700

    Edit image url css html

--- src/components/home/gallery.js
+++ src/components/home/gallery.js
@@ -11,6 +11,9 @@ export class Gallery extends Component {
   editIcon = () => {
 
   }
+  editUrl = () => {
+
+  }
   render() {
     const { homeGallery } = this.props.galleries;
     const { auth } = this.props;
@@ -20,10 +23,12 @@ export class Gallery extends Component {
         <div className="gallery-left">
           {
             homeGallery['0'].map((element, index) => {
+              const urlInput = auth.authenticated ? (<div><label className="gallery-url_input-label" htmlFor="gallery-left-url__input">Image Url</label><input id="gallery-left-url__input" className="da-editable gallery-url__input" placeholder={element.src} onClick={this.editUrl}/></div>) : null;
               return element ? (
                 <div key={index} className="image__container" >
                   <img src={element.src} />
                   <div className="overlay"><div className="overlay__content">
+                    {urlInput}
                     {editIcon}
                     <p>{element.topText}</p><span></span><p>{element.bottomText}</p>
                   </div></div>
@@ -35,10 +40,12 @@ export class Gallery extends Component {
         <div className="gallery-right">
           {
             homeGallery['1'].map((element, index) => {
+              const urlInput = auth.authenticated ? (<div><label className="gallery-url_input-label" htmlFor="gallery-right-url__input">Image Url</label><input id="gallery-right-url__input" className="da-editable gallery-url__input" placeholder={element.src} onClick={this.editUrl}/></div>) : null;
               return element ? (
                 <div key={index} className="image__container" >
                   <img src={element.src} />
                   <div className="overlay"><div className="overlay__content">
+                    {urlInput}
                     {editIcon}
                     <p>{element.topText}</p><span></span><p>{element.bottomText}</p>
                   </div></div>

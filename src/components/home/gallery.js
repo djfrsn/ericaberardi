7c0685374beac898637005f7089a266eee9376dd
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-05 21:03:56 -0700

    Home gallery updates

--- src/components/home/gallery.js
+++ src/components/home/gallery.js
@@ -20,12 +20,6 @@ export class Gallery extends Component {
     toggleGalleryEdit: PropTypes.func.isRequired
   }
   componentWillReceiveProps(nextProps) {
-    if (nextProps.galleries.imageResetMeta.length > 0) {
-      // reset url, imageText inputs for given array of id's
-      // should be image type agnostic
-
-      this.props.clearImageResetMeta();
-    }
     if (nextProps.galleries.toast.type) {
       this.props.clearToast();
       this.props.showToast(nextProps.galleries.toast);
@@ -50,21 +44,7 @@ export class Gallery extends Component {
       }]);
     }, delay);
   }
-  onSaveIconClick = e => {
-    const galleryName = 'homeGallery';
-    const galleryindex = e.currentTarget.parentElement.parentElement.dataset.gallery;
-    const src = this[`placeholder-image-${galleryindex}-src`].value;
-    const topText = this[`placeholder-image-${galleryindex}-topText`].value;
-    const bottomText = this[`placeholder-image-${galleryindex}-bottomText`].value;
-    this.props.saveGalleryImage({galleryindex, galleryName, src, topText, bottomText});
-  }
   onEditIconClick = e => {
-    const galleryName = 'homeGallery';
-    const id = e.currentTarget.parentElement.id;
-    const galleryindex = e.currentTarget.parentElement.dataset.gallery;
-    this.props.toggleGalleryEdit({id, galleryindex, galleryName});
-  }
-  onDeleteIconClick = e => {
     const galleryName = 'homeGallery';
     const id = e.currentTarget.parentElement.id;
     const galleryindex = e.currentTarget.parentElement.dataset.gallery;
@@ -80,7 +60,7 @@ export class Gallery extends Component {
       const galleryindex = e.currentTarget.parentElement.parentElement.dataset.gallery;
       const text = e.currentTarget.value;
       const position = e.currentTarget.dataset.position;
-      this.props.submitGalleryImageUpdates({id, galleryindex, text, position});
+      this.props.submitGalleryImageUpdates({id, galleryindex, text, position, type: 'text'});
     }
   }
   editUrl = e => {
@@ -89,27 +69,16 @@ export class Gallery extends Component {
       const id = e.currentTarget.parentElement.parentElement.id;
       const galleryindex = e.currentTarget.parentElement.parentElement.dataset.gallery;
       const newImageUrl = e.currentTarget.value;
-      this.props.submitGalleryImageUpdates({id, galleryindex, newImageUrl});
+      this.props.submitGalleryImageUpdates({id, galleryindex, newImageUrl, type: 'url'});
     }
   }
   render() {
     const { admin } = this.props;
     const { homeGalleryOne, homeGalleryTwo } = this.props.galleries;
     const { auth } = this.props;
-    const saveIcon = () => { return auth.authenticated ? (<i className="fa fa-check-square-o gallery-edit__icon" aria-hidden="true" onClick={this.onSaveIconClick}></i>) : null; };
     const editIcon = () => { return auth.authenticated ? (<i className="fa fa-pencil-square-o gallery-edit__icon" aria-hidden="true" onClick={this.onEditIconClick}></i>) : null; };
-    const deleteIcon = () => { return auth.authenticated ? (<i className="fa fa-trash-o gallery-edit__icon" aria-hidden="true" onClick={this.onDeleteIconClick}></i>) : null; };
     const urlInput = (el, direction, editing) => { return auth.authenticated && editing ? (<div><label className="gallery-url_input-label" htmlFor={`gallery-${direction}-url__input`}>Image Url</label><input id={`gallery-${direction}-url__input`} className="da-editable gallery-url__input" ref={ref => { this[`urlInput-${el.id}`] = ref; }} placeholder={el.src} onKeyUp={this.editUrl}/></div>) : null; };
-    const imageText = (topText, bottomText, editing, attr = '') => { return auth.authenticated && editing ? (<div className="gallery-edit__image-text"><input type="text" ref={ref => { this[`${attr}-topText`] = ref; }} placeholder={topText} data-position="top" onKeyUp={this.editImageText}/><span></span><input type="text" ref={ref => { this[`${attr}-bottomText`] = ref; }} placeholder={bottomText} onKeyUp={this.editImageText} data-position="bottom"/></div>) : (<div><p className="gallery-image-text">{topText}</p><span></span><p className="gallery-image-text">{bottomText}</p></div>); };
-    const placeholderImages = auth.authenticated && this.props.galleries.placeholderImages.length >= 2 ?
-    this.props.galleries.placeholderImages.map((image, index) => {
-      const galleryindex = index === 0 ? 'homeGalleryOne' : 'homeGalleryTwo';
-      const elUrlInput = urlInput({src: 'Replace w/ img url and click checkmark!'}, image.direction, true, `placeholder-image-${galleryindex}-src`);
-      const elImageText = imageText('topText', 'bottomText', true, `placeholder-image-${galleryindex}`);
-      return (<div key={index} data-gallery={galleryindex} className="image__container gallery-edit__image-placeholder" style={{height: image.height, width: image.width }}><div className="overlay__content" data-placeholder="true">{elUrlInput}{saveIcon()}{elImageText}</div></div>);
-    }) : null;
-    const placeholderImageOne = placeholderImages ? placeholderImages[0] : null;
-    const placeholderImageTwo = placeholderImages ? placeholderImages[1] : null;
+    const imageText = category => { return (<div><p className="gallery-image-text">{category}</p></div>); };
     let pendingNewImagesOne = null;
     let pendingNewImagesTwo = null;
     if (admin.pendingUpdatesRaw.homeGalleryOne) {
@@ -127,7 +96,7 @@ export class Gallery extends Component {
             direction: 'left',
             elUrlInput: urlInput(element, 'left', element.editing),
             elEditIcon: editIcon(),
-            elImageText: imageText(element.topText, element.bottomText, element.editing)
+            elImageText: imageText(element.topText)
           }));
         }
       }
@@ -147,7 +116,7 @@ export class Gallery extends Component {
             direction: 'right',
             elUrlInput: urlInput(element, 'right', element.editing),
             elEditIcon: editIcon(),
-            elImageText: imageText(element.topText, element.bottomText, element.editing)
+            elImageText: imageText(element.topText)
           }));
         }
       }
@@ -163,56 +132,40 @@ export class Gallery extends Component {
               }
               const src = match ? match.src : element.src;
               const topText = match ? match.topText : element.topText;
-              const bottomText = match ? match.bottomText : element.bottomText;
-              const elEditIcon = editIcon();
-              const elUrlInput = urlInput(element, 'left', element.editing);
-              const elImageText = imageText(topText, bottomText, element.editing);
-              const elDeleteIcon = index > 3 ? deleteIcon(element) : null;
+              const elImageText = imageText(topText);
               return element ? (
                 <div key={index} id={element.id} className="image__container" ref={ref => { this[`gallery-left-${index}`] = ref; }} >
                   <img src={src} />
                   <div className="overlay"><div className="overlay__content" id={element.id} data-gallery="homeGalleryOne">
-                    {elUrlInput}
-                    {elEditIcon}
                     {elImageText}
-                    {elDeleteIcon}
                   </div></div>
                 </div>
               ) : null;
             })
           }
           {pendingNewImagesOne}
-          {placeholderImageOne}
         </div>
         <div className="gallery-right">
           {
             homeGalleryTwo.map((element, index) => {
               let match = null;
               if (admin.pendingUpdatesRaw.homeGalleryTwo) {
-                match = admin.pendingUpdatesRaw.homeGalleryTwo[element.id] ? admin.pendingUpdatesRaw.homeGalleryTwo[element.id] : undefined;
+                match = admin.pendingUpdatesRaw.homeGalleryTwo[element.id] ? admin.pendingUpdatesRaw.homeGalleryTwo[element.id] : null;
               }
               const src = match ? match.src : element.src;
               const topText = match ? match.topText : element.topText;
-              const bottomText = match ? match.bottomText : element.bottomText;
-              const elEditIcon = editIcon();
-              const elUrlInput = urlInput(element, 'right', element.editing);
-              const elImageText = imageText(topText, bottomText, element.editing);
-              const elDeleteIcon = index > 2 ? deleteIcon(element) : null;
+              const elImageText = imageText(topText);
               return element ? (
                 <div key={index} id={element.id} className="image__container" ref={ref => { this[`gallery-right-${index}`] = ref; }}>
                   <img src={src} />
                   <div className="overlay"><div className="overlay__content" id={element.id} data-gallery="homeGalleryTwo">
-                    {elUrlInput}
-                    {elEditIcon}
                     {elImageText}
-                    {elDeleteIcon}
                   </div></div>
                 </div>
               ) : null;
             })
           }
           {pendingNewImagesTwo}
-          {placeholderImageTwo}
         </div>
       </div>
     );

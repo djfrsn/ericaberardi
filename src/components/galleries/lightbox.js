0b4426eae39d28a3334af5fe35b5e4ccf1f7082f
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-15 21:59:39 -0700

    Lightbox progress

--- src/components/galleries/lightbox.js
+++ src/components/galleries/lightbox.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { lightboxActions } from 'core/lightbox';
+import * as gUtils from './galleriesUtils';
 
 export default class Lightbox extends Component {
+  static propTypes = {
+    lightbox: PropTypes.object.isRequired
+  }
   render() {
+    const { slides } = this.props.lightbox || {};
+    //const cloud = gUtils.cloudinaryTransform({ type: 'gallery-preview', src: element.src }) || {};
     return (
-      <div className="gallery-lightbox">
+      <div className="gllry-lightbox">
+        <div className="gllry-content">
+          <div className="gllry-controls"><span className="gllry-controls-left">left</span><span className="gllry-controls-right">right</span></div>
+          <div className="gllry-img">
+            <img src={"cloud.src"}/>
+          </div>
+        </div>
       </div>
     );
   }

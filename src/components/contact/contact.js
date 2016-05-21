584433b13f2452281adb6d58833616cd075d9b61
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-20 21:22:34 -0700

    complete html

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,8 +7,21 @@ export class Contact extends Component {
     return (
       <div className="g-row">
         <div className="g-col" >
-          <div className="under__construction">
-          [ Under Construction ]
+          <div className="contact__container">
+            <div className="contact__form">
+              <h1>Nice to meet you!</h1>
+              <input type="text" className="contact__name"/>
+              <input type="text" className="contact__email"/>
+              <textarea name="message" value="Message*" />
+              <button className="contact__send">Send</button>
+            </div>
+            <div className="contact__social">
+              <a href="" className="contact__email-link">ericamarieowen@yahoo.com</a>
+              <i className="fa fa-facebook-official" aria-hidden="true"></i>
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+              <i className="fa fa-google-plus" aria-hidden="true"></i>
+            </div>
           </div>
         </div>
       </div>

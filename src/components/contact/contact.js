745be775c57dce5fab1f2b4fd16abe9ad1c2cd85
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-21 18:17:48 -0700

    Horizontal icons for mobile

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -17,10 +17,12 @@ export class Contact extends Component {
             </div>
             <div className="contact__social">
               <a href="mailto:ericamarieowen@yahoo.com" className="contact__email-link">ericamarieowen@yahoo.com</a>
-              <a href="#" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
-              <a href="#" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-              <a href="#" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
-              <a href="#" className="contact__social_link"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
+              <div className="contact__social_icons">
+                <a href="#" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                <a href="#" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="#" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#" className="contact__social_link"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
+              </div>
             </div>
           </div>
         </div>

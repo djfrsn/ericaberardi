4bd2ffb315e1fb4b84e16191d7184b857bb57522
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-21 18:36:33 -0700

    cleanup

--- src/core/admin/actions.js
+++ src/core/admin/actions.js
@@ -3,12 +3,7 @@ import {
   LOAD_PENDING_UPDATES,
   PUBLISH_SUCCESS,
   PUBLISH_ERROR,
-  CREATE_TASK_ERROR,
-  CREATE_TASK_SUCCESS,
-  CLEAR_UPDATES_ERROR,
-  DELETE_TASK_SUCCESS,
-  UPDATE_TASK_ERROR,
-  UPDATE_TASK_SUCCESS
+  CLEAR_UPDATES_ERROR
 } from './action-types';
 import Firebase from 'firebase';
 import { FIREBASE_URL } from '../../config';
@@ -111,84 +106,4 @@ export function deletePublishUpdates(update) {
     const { firebase } = getState();
     deleteAdminChanges(firebase, update, dispatch);
   };
-}
-
-export function createTask(title) {
-  return (dispatch, getState) => {
-    const { auth, firebase } = getState();
-
-    firebase.child(`tasks/${auth.id}`)
-      .push({completed: false, title}, error => {
-        if (error) {
-          console.error('ERROR @ createTask :', error); // eslint-disable-line no-console
-          dispatch({
-            type: CREATE_TASK_ERROR,
-            payload: error
-          });
-        }
-      });
-  };
-}
-
-
-export function undeleteTask() {
-  return (dispatch, getState) => {
-    const { auth, firebase, tasks } = getState();
-    const task = tasks.deleted;
-
-    firebase.child(`tasks/${auth.id}/${task.key}`)
-      .set({completed: task.completed, title: task.title}, error => {
-        if (error) {
-          console.error('ERROR @ undeleteTask :', error); // eslint-disable-line no-console
-        }
-      });
-  };
-}
-
-
-export function updateTask(task, changes) {
-  return (dispatch, getState) => {
-    const { auth, firebase } = getState();
-
-    firebase.child(`tasks/${auth.id}/${task.key}`)
-      .update(changes, error => {
-        if (error) {
-          console.error('ERROR @ updateTask :', error); // eslint-disable-line no-console
-          dispatch({
-            type: UPDATE_TASK_ERROR,
-            payload: error
-          });
-        }
-      });
-  };
-}
-
-
-export function registerListeners() {
-  return (dispatch, getState) => {
-    const { auth, firebase } = getState();
-    const ref = firebase.child(`tasks/${auth.id}`);
-
-    ref.on('child_added', snapshot => dispatch({
-      type: CREATE_TASK_SUCCESS,
-      payload: recordFromSnapshot(snapshot)
-    }));
-
-    ref.on('child_changed', snapshot => dispatch({
-      type: UPDATE_TASK_SUCCESS,
-      payload: recordFromSnapshot(snapshot)
-    }));
-
-    ref.on('child_removed', snapshot => dispatch({
-      type: DELETE_TASK_SUCCESS,
-      payload: recordFromSnapshot(snapshot)
-    }));
-  };
-}
-
-
-function recordFromSnapshot(snapshot) {
-  let record = snapshot.val();
-  record.key = snapshot.key();
-  return record;
 }

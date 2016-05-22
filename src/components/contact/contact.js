93b6d337ebf3697d80e30b6f4c7c4f890991e963
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-21 17:00:00 -0700

    Contact Page html & css complete

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -9,18 +9,18 @@ export class Contact extends Component {
         <div className="g-col" >
           <div className="contact__container">
             <div className="contact__form">
-              <h1>Nice to meet you!</h1>
-              <input type="text" className="contact__name"/>
-              <input type="text" className="contact__email"/>
-              <textarea name="message" value="Message*" />
+              <h2>Nice to meet you!</h2>
+              <input type="text" placeholder="Name" className="contact__name"/>
+              <input type="text" placeholder="Email" className="contact__email"/>
+              <textarea name="message" placeholder="Message" />
               <button className="contact__send">Send</button>
             </div>
             <div className="contact__social">
-              <a href="" className="contact__email-link">ericamarieowen@yahoo.com</a>
-              <i className="fa fa-facebook-official" aria-hidden="true"></i>
-              <i className="fa fa-twitter" aria-hidden="true"></i>
-              <i className="fa fa-instagram" aria-hidden="true"></i>
-              <i className="fa fa-google-plus" aria-hidden="true"></i>
+              <a href="mailto:ericamarieowen@yahoo.com" className="contact__email-link">ericamarieowen@yahoo.com</a>
+              <a href="#" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+              <a href="#" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="#" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+              <a href="#" className="contact__social_link"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
             </div>
           </div>
         </div>
@@ -28,5 +28,4 @@ export class Contact extends Component {
     );
   }
 }
-
 export default connect(null, authActions)(Contact);

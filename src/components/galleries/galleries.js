abc923b75c038c264be259ad5eb891f1d4c6af91
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-26 21:09:37 -0700

    Upload img preview

--- src/components/galleries/galleries.js
+++ src/components/galleries/galleries.js
@@ -10,6 +10,7 @@ import galleryCategories from './galleryCategories';
 import galleryImages from './galleryImages';
 import Lightbox from './lightbox';
 import Masonry from 'react-masonry-component';
+import Dropzone from 'react-dropzone';
 
 const masonryOptions = {
   transitionDuration: 500,
@@ -30,6 +31,7 @@ export class Galleries extends Component {
   state = {
     gallery: [],
     categories: [],
+    files: [],
     loadImagesSeq: true
   }
   componentWillMount() {
@@ -56,6 +58,18 @@ export class Galleries extends Component {
     this.unbindImagesLoaded = true;
     gUtils.unbindImagesLoaded(this.galleryContainer);
     window.onresize = () => {}; // remove listener
+  }
+  onDrop(files) {
+    console.log('Received files: ', this.state.isDragReject);
+    if (!this.state.isDragReject) {
+      this.props.onDropAccept(files);
+    }
+  }
+  onDropAccept = files => {
+    this.setState({
+      ...this.state,
+      files: files
+    });
   }
   showLightbox = e => {
     e.preventDefault();
@@ -89,6 +103,15 @@ export class Galleries extends Component {
               >
                 {galleryImages({gallery, scope: this})}
             </Masonry>
+          </div>
+          <div className="gallery__dropzone">
+            <Dropzone accept="image/jpeg, image/png" onDropAccept={this.onDropAccept} onDrop={this.onDrop}>
+              <div>Try dropping some files here, or click to select files to upload.</div>
+            </Dropzone>
+            {this.state.files.length > 0 ? <div>
+                <h2>Uploading {this.state.files.length} files...</h2>
+                <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
+                </div> : null}
           </div>
           <Lightbox/>
         </div>

539b936d128a55a5c1807ba73c553a794bb26b10
Dennis Jefferson <dennismjefferson@gmail.com>
2016-05-29 13:24:06 -0700

    Fix lint errors

--- src/components/admin/pendingUpdatesList.js
+++ src/components/admin/pendingUpdatesList.js
@@ -9,14 +9,16 @@ export default opts => {
     let categoryContainer;
     // Create list items
     forIn(updates, (update, subCategory) => {
-      update.length > 0 ? pendingUpdates.push((
-        <p key={`pendingUpdates-${subCategory}`} className="admin-pending_paragraph">{subCategory} - {update.length}</p>
-      )) : null;
+      if (update.length > 0) {
+        pendingUpdates.push((
+          <p key={`pendingUpdates-${subCategory}`} className="admin-pending_paragraph">{subCategory} - {update.length}</p>
+        ));
+      }
     });
 
     if (pendingUpdates) {
       categoryContainer = (
-        <div key={`pendingUpdates-${category}`}  className="category__container"><h3>{category}</h3>{pendingUpdates}</div>
+        <div key={`pendingUpdates-${category}`} className="category__container"><h3>{category}</h3>{pendingUpdates}</div>
         );
       // Push new container onto list of components
       pendingUpdatesList.push(categoryContainer);

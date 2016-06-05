8d3c24c45762092f5f185c8a94faa2c367edca9d
Dennis Jefferson <dennismjefferson@gmail.com>
2016-06-05 12:33:55 -0700

    gallery delete image progress

--- src/core/galleries/actions.js
+++ src/core/galleries/actions.js
@@ -110,52 +110,49 @@ export function toggleGalleryDelete() {
 export function onGalleryDeleteImages(successCallback) {
   return (dispatch, getState) => {
     const { firebase, galleries } = getState();
-    const aGllrys = activeGalleries(galleries);
-    const aGllrysKey = aGllrys.galleriesKey;
-    const gllrys = aGllrys.galleries;
-    const imagesToDelete = [];
     const storage = firebase.storage();
-    const imagesDeletedCount = 0;
+    let imagesDeletedCount = 0;
     const taggedForDeleteCount = galleries.taggedForDeleteCount;
+    let success = false;
 
-    forIn(gllrys, gallery => {
-      gallery.forEach(image => {
+    forIn(galleries.images, gallery => {
+      forIn(gallery, image => {
         if (image.shouldDelete) {
-          // TODO: we should Explore having a single galleries state
-          // TODO: call if all files have been deleted....keep count since we know how many are selected
-          // set a 30 sec timeout to run and see if success callback failed, then warn that some imgs may not have been deleted
-          // if (imagesDeletedCount === taggedForDeleteCount) {
-          // if (utils.isFunction(successCallback)) {
-             //   successCallback(); // TODO: break utils into named functions
-            // }
-          // }
+          // call if all files have been deleted....keep count since we know how many are selected
+          // TODO: place this inside img delete callback
+          imagesDeletedCount++;
+          if (imagesDeletedCount === taggedForDeleteCount) {
+            success = true;
+            successCallback();
+          }
           // TODO: for each file that is deleted the meta data must also be removed from firebase
-          // make api call here to delete files
-          // TODO: shouldDelete imgs get deleted else push those images into a local collection
-          imagesToDelete.push(image.id);
-          const storageRef = storage.ref();
-          // Create a reference to the file to delete
-          const imageRef = storageRef.child(image.fullPath);
-
-          // Delete the file
-          // imageRef.delete().then(function() {
-          //   // File deleted successfully
-          //  imagesDeletedCount++;
-          // }).catch(function(error) {
-          //   // Uh-oh, an error occurred!
+          console.log(image, `${ENV}/galleries/${image.categoryId}/${image.id}`);
+          // firebase.database().ref(`${ENV}/galleries/${image.categoryId}/${image.id}`).set(null).then(() => {
+          //   const storageRef = storage.ref();
+          //   // Create a reference to the file to delete
+          //   const imageRef = storageRef.child(image.fullPath);
+          //   imageRef.delete().then(function() {
+          //     // File deleted successfully
+          //     imagesDeletedCount++;
+          //   }).catch(function(error) {
+          //     // Uh-oh, an error occurred!
+          //     // log errors to env/logs/errors/galleries/shouldDelete/image.id = image
+          //   });
+          // }).catch(error => {
+          //   // err :(
+          //   // TODO: dispatch alert for image deletion error...pass name of image along... in msg
           // });
         }
       });
-      // NOTE: NEVER STORE ARRAYS IN FIREBASE!!!!!!!!!!!
-      // TODO: loop galleries/pending-galleries and check imagesToDelete id's to remove meta data from galleries & pending-galleries
-      const inActiveGalleries = aGllrysKey === 'galleries' ? 'galleries' : 'pending-galleries';
-      forIn(inActiveGalleries, gallery => {
-        gallery.forEach(image => {
-          if (image.shouldDelete) {
-            // TODO: shouldDelete imgs get deleted else push those images into a local collection
-          }
-        });
-      });
+
+      setTimeout(() => {
+        if (!success) {
+          // TODO: set a 45 sec timeout to run and see if success callback failed, then warn that some imgs may not have been deleted
+          successCallback();
+        }
+      }, 45000);
+      // TODO: loop galleries and check imagesToDelete id's to remove meta data from galleries & pending-galleries
+
     });
 
     dispatch({

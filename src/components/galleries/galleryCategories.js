127aee1212694906f2ed1a1f584d22c3045252c9
Dennis Jefferson <dennismjefferson@gmail.com>
2016-06-19 17:34:28 -0700

    Categories w/ orderby

--- src/components/galleries/galleryCategories.js
+++ src/components/galleries/galleryCategories.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 import forIn from 'lodash.forin';
+import forEach from 'lodash.foreach';
+import orderBy from 'lodash.orderBy';
 
 function getCategories(opts) {
   const authenticated = opts.scope.props.auth.authenticated;
@@ -14,6 +16,13 @@ function getCategories(opts) {
     );
   });
 
+  let sortedOrderByCategories = []; // create new array with value as top lvl prop for easy sorting
+  forEach(categoryOptions, option => {
+    sortedOrderByCategories.push({...option, value: option.props.value});
+  });
+  // Keep option values sorted
+  sortedOrderByCategories = orderBy(sortedOrderByCategories, 'value', 'asc');
+
   forIn(categoriesProps, (category, key, list) => {
     const galleryLink = 'gallery__link';
     const defaultGallery = list[Object.keys(list)[0]];
@@ -22,16 +31,22 @@ function getCategories(opts) {
     const protectedCategory = category.pending && !authenticated;
     if (category && !protectedCategory) {
       categories.push(
-        <li key={key}>
+        <li key={key} orderby={category.orderby}>
           {authenticated ? <div className="select-style"><select name="categoryOrderby" className="gallery_category_orderby" value={category.orderby} onChange={opts.scope.onChangeGalleryCategoryOrder}>
-            {categoryOptions}
+            {sortedOrderByCategories}
           </select></div> : null}
           <Link to={`/galleries/${category.category}`} className={className}>{category.category}</Link>
         </li>
       );
     }
   });
-  return categories;
+
+  let orderedCategories = [];
+  forEach(categories, category => {
+    orderedCategories.push({...category, orderBy: category.props.orderby});
+  });
+
+  return orderBy(orderedCategories, 'orderBy', 'asc');
 }
 
 export default opts => {

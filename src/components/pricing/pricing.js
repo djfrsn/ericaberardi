92c073489f2758d50c8f01d09ab58313f714cbd5
Dennis Jefferson <dennismjefferson@gmail.com>
2016-06-19 20:17:09 -0700

    pricing base

--- src/components/pricing/pricing.js
+++ src/components/pricing/pricing.js
@@ -7,8 +7,25 @@ export class About extends Component {
     return (
       <div className="g-row">
         <div className="g-col" >
-          <div className="under__construction">
-          [ Under Construction ]
+          <div className="pricing__container">
+            <ul className="pricing__link_wrapper">
+              <li className="pricing_link_li"><a href="" className="pricing__link">Personal and Family Portraits</a></li>
+              <li className="pricing_link_li"><a href="" className="pricing__link">Events(Sports, Birthdays, Showers)</a></li>
+              <li className="pricing_link_li"><a href="" className="pricing__link">Commercial</a></li>
+            </ul>
+            <div className="pricing__packages_wrapper">
+              <div className="pricing__package">
+                <ul className="pricing__list">
+                  <li>Package A</li>
+                  <li>$175</li>
+                  <li>20 minute session</li>
+                  <li>Proofs of 10-15 images</li>
+                  <li>Choose 5 images</li>
+                  <li>5 jpegs formatted for print and digital use</li>
+                  <li>Each additional jpeg for $30</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -17,3 +34,4 @@ export class About extends Component {
 }
 
 export default connect(null, authActions)(About);
+// [ Under Construction ]

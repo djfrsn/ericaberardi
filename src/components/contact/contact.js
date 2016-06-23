08aac4e0808deba2ce1d3877c6f17897ae649656
Dennis Jefferson <dennismjefferson@gmail.com>
2016-06-22 21:48:02 -0700

    Add social link

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -87,7 +87,7 @@ export class Contact extends Component {
               <div className="contact__social_icons">
                 <a href="https://www.facebook.com/ericaberardiphotography/" target="_blank" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
                 <a href="#" target="_blank" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-                <a href="#" target="_blank" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/ericaberardiphotography/?hl=en" target="_blank" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
                 <a href="#" target="_blank" className="contact__social_link"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
               </div>
             </div>

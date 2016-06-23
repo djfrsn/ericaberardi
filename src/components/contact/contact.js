7d07a1fa2acfe3e097b40ccef1f6d20ef24760f9
Dennis Jefferson <dennismjefferson@gmail.com>
2016-06-22 20:25:52 -0700

    Update social links

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -85,10 +85,10 @@ export class Contact extends Component {
             <div className="contact__social">
               <a href="mailto:ericaberardiphotography@gmail.com" className="contact__email-link">ericaberardiphotography@gmail.com</a>
               <div className="contact__social_icons">
-                <a href="#" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
-                <a href="#" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-                <a href="#" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="#" className="contact__social_link"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/ericaberardiphotography/" target="_blank" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
+                <a href="#" target="_blank" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="#" target="_blank" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#" target="_blank" className="contact__social_link"><i className="fa fa-google-plus" aria-hidden="true"></i></a>
               </div>
             </div>
           </div>

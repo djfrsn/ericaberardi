a905057fcb7d1d9853afa4a37366a77a064178e2
Dennis Jefferson <dennismjefferson@gmail.com>
2016-07-23 15:13:15 -0700

    Update for caching

--- src/lava.js
+++ src/lava.js
@@ -8,7 +8,7 @@ import findIndex from 'lodash.findindex';
 * General
 */
 
-// TODO: this function should take a param for what index of a url you would like
+// IMPROVEMENT: this function should take a param for what index of a url you would like
 // no matter how long i.e val = '1/2/3'; parsePath(val, 3) === 3 // true
 export function parsePath(val) { // takes paths like galleries/sports || /galleries/sports and returns sports
   let path = '/';
@@ -28,42 +28,9 @@ export function parsePath(val) { // takes paths like galleries/sports || /galler
 * Database
 */
 
-// Helpful for converting objects and their list of key values into an object array
-export function fObjectToObjectArray(fObject) {
-  let objectArray = {};
-  forIn(fObject, (value, key) => {
-    let newKey = objectArray[key] = []; // create array for fObject key
-    forIn(value, (val, key) => {
-      val.fObjectKey = key; // allow child objects to be referenced by key
-      newKey.push(val); // push objects into fObject key
-    });
-  });
-  // input
-  // { commercial: { foo: { foo: bar }, bar: { foo: bar } } }
-  // output
-  // { commercial: [{ foo: bar }, { foo: bar }] }
-  return objectArray;
-}
-
-// Helpful for merging ObjectArrays
-export function mergeObjectArrays(destination, source) {
-  let mergedObjectArray = {};
-  forIn(destination, (list, key) => {
-    let newKey = mergedObjectArray[key] = []; // create key for mergedObjectArray
-    newKey.push(...list); // push list of key/val pairs onto array
-    if (source[key]) { // check if source[key] existing ....
-      forIn(source[key], val => {
-        const isNewVal = findIndex(newKey, { id: val.id }) === -1; // returns true if val isn't found in newKey
-        if (isNewVal) {
-          newKey.push(val); // push matched source object children into newKey
-        }
-      });
-    }
-  });
-  // input
-  // destination: { commercial: [{ foo: bar }, { foo: bar }], sports: [{ foo: bar }, { foo: bar }] }
-  // source: { commercial: [{ foo: bar }], portraits: [{ foo: bar }, { foo: bar }] }
-  // output
-  // { commercial: [{ foo: bar }, { foo: bar }, { foo: bar }], sports: [{ foo: bar }, { foo: bar }], portraits: [{ foo: bar }, { foo: bar }] }
-  return mergedObjectArray;
+export function localStorage(category, data) {
+  // strategy
+  // when data changes we store data by route
+  // on page load we hydrate the store with data from local storage
+  // 3 day cache expiration
 }

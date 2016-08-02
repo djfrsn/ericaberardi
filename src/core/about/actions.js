eff22caf8f2f0f5408907c56e4c43ea3d3ac6616
Dennis Jefferson <dennismjefferson@gmail.com>
2016-08-01 22:39:47 -0700

    Add about file storage

--- src/core/about/actions.js
+++ src/core/about/actions.js
@@ -28,33 +28,32 @@ export function editAbout(opts) {
         const filedata = data[Object.keys(data)[0]];
         const pendingfile = filedata.pendingfile;
         // upload pending file
-      if (pendingfile) {
-        // ensure files aren't above 850k
-        if (pendingfile.size < 600000) {
-        // Should be good o_O
-        debugger
-          // const storageRef = storage.ref().child(filedata.id); // name of folder
-          // const fileRef = storageRef.child(pendingfile.name); // name of file
-          // const uploadFile = fileRef.put(pendingfile);
+        if (pendingfile) {
+            // ensure files aren't above 850k
+          if (pendingfile.size < 600000) {
+            const storageRef = storage.ref().child(filedata.id); // name of folder
+            const fileRef = storageRef.child(pendingfile.name); // name of file
+            const uploadFile = fileRef.put(pendingfile);
 
-          // uploadFile.on('state_changed', null, null, () => {
-          //   // Handle successful uploads on complete
-          //   const id = firebase.database(`about/${category}`).ref().child(`${filedata.id}`).push().key;
-          //   const src = uploadFile.snapshot.downloadURL;
-          //   const docMeta = uploadFile.snapshot.metadata;
-          //   const { contentType, downloadURLs, fullPath, name, size, timeCreated } = docMeta;
-          //   const imageData = { id, src, [`${category}Id`]: filedata.id, contentType, downloadURLs, fullPath, name, size, timeCreated };
-          //   const newFileData = {
-          //     ...filedata,
-          //     pendingfile: imageData,
-          //     pendingfiledeleted: null
-          //   };
+            uploadFile.on('state_changed', null, null, () => {
+              // Handle successful uploads on complete
+              const id = firebase.database(`about/${category}`).ref().child(`${filedata.id}`).push().key;
+              const src = uploadFile.snapshot.downloadURL;
+              const docMeta = uploadFile.snapshot.metadata;
+              const { contentType, downloadURLs, fullPath, name, size, timeCreated } = docMeta;
+              const imageData = { id, src, [`${category}Id`]: filedata.id, contentType, downloadURLs, fullPath, name, size, timeCreated };
+              const newFileData = {
+                ...filedata,
+                pendingsrc: src,
+                pendingfile: imageData,
+                pendingfiledeleted: null
+              };
 
-          //   // update meta
-          //   database.ref(`about/${category}/${filedata.id}`).set(newFileData);
-          // });
+              // update meta
+              database.ref(`about/${category}/${filedata.id}`).set(newFileData);
+            });
+          }
         }
-      }
       }
     });
   };
@@ -66,8 +65,8 @@ export function deleteAboutFile(prevAbout) {
     const storage = firebase.storage();
     const storageRef = storage.ref();
     // Create a reference to the file to delete
-    // const fileRef = storageRef.child(prevAbout.pendingfile.fullPath);
-    // fileRef.delete();
+    const fileRef = storageRef.child(prevAbout.pendingfile.fullPath);
+    fileRef.delete();
   };
 }
 

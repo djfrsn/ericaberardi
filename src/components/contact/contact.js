4a0f77e96bcea6ee61a5b67003703b715d28f1a7
Dennis Jefferson <dennismjefferson@gmail.com>
2016-08-03 18:12:08 -0700

    Add contact page inline editing

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { authActions } from 'core/auth';
 import { toastActions } from 'core/toast';
 import { contactActions } from 'core/contact';
+import { textEditCanvas } from 'helpers/textEdit';
 import forEach from 'lodash.foreach';
 import delay from 'lodash.delay';
 import socialIcons from '../partials/socialIcons';
@@ -26,6 +27,7 @@ const initialState = {
 
 export class Contact extends Component {
   static propTypes = {
+    auth: PropTypes.object.isRequired,
     clearContactToast: PropTypes.func.isRequired,
     contact: PropTypes.object.isRequired,
     sendEmail: PropTypes.func.isRequired,
@@ -80,19 +82,72 @@ export class Contact extends Component {
     const { contactName, contactEmail, contactSubject, contactMessage } = this.state;
     this.props.sendEmail({ contactName, contactEmail, contactSubject, contactMessage});
   }
+  textEditTargetReverted = opts => {
+    // let dispatchType;
+    // let valueChanged = false;
+    // let data = {};
+    // if (opts.meta.type === 'about') {
+    //   dispatchType = 'editAbout';
+    //   const parsedAbout = parseAbout({ newAbout: opts.data, prevAbout: this.props.about, scope: this });
+    //   valueChanged = !parsedAbout.equal;
+    //   data = { about: parsedAbout.newAbout };
+    // }
+
+    // if (valueChanged) {
+    //   this.props[dispatchType](data); // update db with changes
+    // }
+    this.setState({ ...this.state, isEditing: false });
+  }
+  editContact = e => {
+    textEditCanvas({
+      e,
+      className: 'textEdit-contact',
+      inputParent: 'li',
+      callback: this.textEditTargetReverted,
+      meta: { type: 'contact' }
+    });
+    this.setState({ ...this.state, isEditing: true});
+  }
+  onIconSrcChange = e => {
+    const currentTarget = e.currentTarget;
+    this.setState({
+      ...this.state,
+      [currentTarget.dataset.type]: {
+        ...this.state.profilepicture,
+        [`${currentTarget.dataset.type}-src-${currentTarget.dataset.id}`]: currentTarget.value
+      }
+    });
+  }
   render() {
+    const { auth } = this.props;
+    const contact = { hello: 'hi' };
+    const authenticated = auth.authenticated;
     const { nameError, emailError, subjectError, textareaError, allFieldsHaveValues } = this.state;
     const contactNameClass = classNames({ ['contact__name']: true, ['eb__input_error']: nameError });
     const contactEmailClass = classNames({ ['contact__email']: true, ['eb__input_error']: emailError });
     const contactSubjectClass = classNames({ ['contact__subject']: true, ['eb__input_error']: subjectError });
     const contactTextAreaClass = classNames({ ['contact__textarea']: true, ['eb__input_error']: textareaError });
     const recaptchaClass = classNames({ ['g-recaptcha']: true, ['hidden']: !allFieldsHaveValues });
+    let socialIconsEditingInputs = null;
+    if (this.state.isEditing) {
+      socialIconsEditingInputs = (<div>
+        <label htmlFor="facebookicon">Facebook Link</label>
+        <input type="text" id="facebookicon" data-type="facebook" onChange={this.onIconSrcChange}/>
+        <label htmlFor="twittericon">Twitter Link</label>
+        <input type="text" id="twittericon" data-type="twitter" onChange={this.onIconSrcChange}/>
+        <label htmlFor="instagramicon">Instagram Link</label>
+        <input type="text" id="instagramicon" data-type="instagram" onChange={this.onIconSrcChange}/>
+        <label htmlFor="googleicon">Google + Link</label>
+        <input type="text" id="googleicon" data-type="google" onChange={this.onIconSrcChange}/>
+      </div>);
+    }
     return (
       <div className="g-row">
         <div className="g-col" >
           <div className="contact__container">
-            <form onSubmit={this.sendEmail} className="contact__form">
-              <h2 className="contact__form_title">Nice to meet you!</h2>
+           {authenticated && Object.keys(contact).length > 0 ? <i onClick={this.editContact} className="fa fa-pencil-square-o page_edit_icon" aria-hidden="true"></i> : null}
+            <form onSubmit={this.sendEmail} className="contact__form" data-textedittargetparent>
+              <h2 className="contact__form_title" data-textedittarget data-texteditid="contact__form_title">Nice to meet you!</h2>
               <input data-contact-type="Name" type="text" placeholder="Name" className={contactNameClass} value={this.state.contactName} onChange={this.handleChange} ref={ref => { this.contactName = ref; }}/>
               <input data-contact-type="Email" type="text" placeholder="Email" className={contactEmailClass} value={this.state.contactEmail} onChange={this.handleChange} ref={ref => { this.contactEmail = ref; }}/>
               <input data-contact-type="Subject" type="text" placeholder="Subject" className={contactSubjectClass} value={this.state.contactSubject} onChange={this.handleChange} ref={ref => { this.contactSubject = ref; }}/>
@@ -100,9 +155,10 @@ export class Contact extends Component {
               <div className={recaptchaClass} data-sitekey="6LeaQyQTAAAAADVFB5FGzAv-0d6Qsf_ZJoznUq1c"></div>
               <button onClick={this.sendEmail} className="contact__send">Send</button>
             </form>
-            <div className="contact__social">
-              <a href="mailto:ericaberardiphotography@gmail.com" className="contact__email-link">ericaberardiphotography@gmail.com</a>
-              {socialIcons()}
+            <div className="contact__social" data-textedittargetparent>
+              <a href="mailto:ericaberardiphotography@gmail.com" className="contact__email-link" data-textedittarget data-texteditid="contact__email-link">ericaberardiphotography@gmail.com</a>
+              {socialIcons({ className: 'contact__page'})}
+              {socialIconsEditingInputs}
             </div>
           </div>
         </div>
@@ -112,6 +168,7 @@ export class Contact extends Component {
 }
 
 export default connect(state => ({
+  auth: state.auth,
   contact: state.contact,
   toast: state.toast
 }), Object.assign({}, authActions, toastActions, contactActions))(Contact);
--- src/components/partials/socialIcons.js
+++ src/components/partials/socialIcons.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default () => {
+export default (opts = {}) => {
+  const iconsWrapperClass = opts.className ? `${opts.className} contact__social_icons` : 'contact__social_icons';
   return (
-    <div className="contact__social_icons">
+    <div className={iconsWrapperClass}>
       <a href="https://www.facebook.com/ericaberardiphotography/" target="_blank" className="contact__social_link"><i className="fa fa-facebook-official" aria-hidden="true"></i></a>
       <a href="#" className="contact__social_link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
       <a href="https://www.instagram.com/ericaberardiphotography/?hl=en" target="_blank" className="contact__social_link"><i className="fa fa-instagram" aria-hidden="true"></i></a>
